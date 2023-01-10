ef93f443cab7c3375fd9d77ac7b560f028c31d25
uwimanax <uwimanaxavi10@gmail.com>
2023-01-10 11:28:53 +0200

    creating dev blanche

--- app.py
+++ app.py
@@ -3,3 +3,12 @@ main  = FastAPI()
 @main.get('/') #this the path/route
 def index():
     return "This is the front page"
+@main.get("/abaut")
+def abaut():
+    return "abaut page"
+
+
+#creating dev blanche
+@main.get("/dev")
+def dev():
+    return "development page"
